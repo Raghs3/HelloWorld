ce2f07da6da7bda19f334491aa98a850265a8f3d
Raghs3 <tinku.minku2803@gmail.com>
2024-11-18 16:33:42 +0530

    Initial Commit

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,5 +1,5 @@
 print('Hello, World!')  # can use ' or "  # string literal is argument  # print is function
 print(1+2)  # can print calculation results
 print(7*6)
-print()  # prints blank line, when calling a function '()' is needed
+print()  # prints blank line # when calling a function '()' is needed
 print("The end", "or is it?", "keep watching to know more about Python", 3)  # 4 argument, 3 str, 1 var, auto spacing
