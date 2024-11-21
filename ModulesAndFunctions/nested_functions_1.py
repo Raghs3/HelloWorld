2bc82539395e4213b7bd1ad60fc64165277f6f0a
Raghs3 <tinku.minku2803@gmail.com>
2024-11-21 09:02:43 +0530

    Day 4 - scopes and free variables

--- ModulesAndFunctions/nested_functions_1.py
+++ ModulesAndFunctions/nested_functions_1.py
@@ -1,13 +1,19 @@
 def greet_pythons(items: list) -> None:
     greeting = 'Hello'
+    print(f'The ID of `greeting` in `greet_pythons` is {id(greeting)}')
     # variables defined in enclosing scope become available in inner scopes
     def make_greeting(item: str) -> str:
+        greeting = 'Hi'  # now greeting doesn't refer to same object as outer function
+        print(f'The ID of `greeting` in `make_greeting` id {id(greeting)}')
         return f'{greeting} {item}'  # greeting is in enclosing scope but because of free variable it will be found in local
         # greeting also exist in make_greeting namespace (free variables)
     for item in items:
         print(make_greeting(item))
+    print(f'Inside greet_pythons, `greeting` is {greeting}')
+    print(f'The ID of `greeting` in `greet_pythons` is {id(greeting)}')
 # global variable cant be a free variable
 
-names = ['John', 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
+names = ['John']  #, 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
 
 greet_pythons(names)
+# when you bind a free variable to a new value, new variable is created
