fa3d513a6e8a09caeca8779a51e7bed51c10512a
Raghs3 <tinku.minku2803@gmail.com>
2024-11-21 08:52:30 +0530

    Day 4 - scopes

--- ModulesAndFunctions/nested_functions_1.py
+++ ModulesAndFunctions/nested_functions_1.py
@@ -1,12 +1,12 @@
 def greet_pythons(items: list) -> None:
     greeting = 'Hello'
-
+    # variables defined in enclosing scope become available in inner scopes
     def make_greeting(item: str) -> str:
-        return f'{greeting} {item}'  # greeting is enclosing scope?
-
+        return f'{greeting} {item}'  # greeting is in enclosing scope but because of free variable it will be found in local
+        # greeting also exist in make_greeting namespace (free variables)
     for item in items:
         print(make_greeting(item))
-
+# global variable cant be a free variable
 
 names = ['John', 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
 
