60f45318bddf2d24348f43f496b51d51661960b1
Raghs3 <tinku.minku2803@gmail.com>
2024-11-21 07:03:36 +0530

    Day 4 - local, global variables

--- ModulesAndFunctions/import_turtle.py
+++ ModulesAndFunctions/import_turtle.py
@@ -29,9 +29,11 @@ def encircled_square(length: int) -> None:
     turtle.right(135)
     turtle.circle(radius)
     turtle.left(135)  # takes same time, draws pattern differently and leaves turtle in same dirn as it started
+    print(f"Inside function, namespace is: {dir()}")  # gives names in a list
+    print(f"locals: {locals()}")  # shows the namespace dict, i.e. values too
 
 
-# encircled_square(300)
+encircled_square(300)
 # turtle.speed('fast')
 # for s in range(72):
 #     encircled_square(120)
@@ -44,3 +46,5 @@ print(dir())  # output is a list containing all the names that exist in our name
 # namespace is a container for variable names
 # score is where that variable exists
 # scope of a variable means the places in our code where it can be used
+# or, scope is where an object exists or where it can be used
+# modules, functions and classes can create a new scope
