238987c86467eaee07ca3fac87265343e7fe9e7f
Raghs3 <tinku.minku2803@gmail.com>
2024-11-22 07:19:40 +0530

    Day 5 - how not to name variables

--- ModulesAndFunctions/better_code.py
+++ ModulesAndFunctions/better_code.py
@@ -1,8 +1,16 @@
-def area_of_square(length: float):
-    global area
-    area = length * length
+# def area_of_square(length: float):
+#     global area
+#     area = length * length
+#
+#
+# area_of_square(30)
+# print(f'The area is {area}')
+# # THIS IS EVEN WORSE
 
 
-area_of_square(30)
+def area_of_square(length: float) -> float:
+    return length * length
+
+
+area = area_of_square(30)
 print(f'The area is {area}')
-# THIS IS EVEN WORSE
