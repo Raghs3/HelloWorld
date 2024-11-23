12de6729fbb5bd510cb876c78721830537658433
Raghs3 <tinku.minku2803@gmail.com>
2024-11-23 18:00:25 +0530

    Day 6 - import *

--- ModulesAndFunctions/import_turtle.py
+++ ModulesAndFunctions/import_turtle.py
@@ -7,7 +7,8 @@
 # turtle.right(90)
 # turtle.done()
 
-import turtle
+# import turtle
+from turtle import *  # every name that doesn't begin with one or more underscore will be imported into namespace
 # import math
 from math import radians, cos
 
@@ -15,8 +16,8 @@ from math import radians, cos
 def square(length: int) -> None:
     """Draws a square of length `length`"""
     for side in range(4):
-        turtle.forward(length)
-        turtle.right(90)
+        forward(length)  # turtle.forward(length)
+        right(90)  # turtle.right(90)
 
 
 def encircled_square(length: int) -> None:
@@ -26,20 +27,20 @@ def encircled_square(length: int) -> None:
     square(length)  # doesn't find in local namespace, then looks in enclosing scope which is global scope in this case
     angle = radians(45)  # math.radians(45)  # math prefix not req as we imported them directly to namespace
     radius = length * cos(angle)  # math.cos(angle)
-    turtle.right(135)
-    turtle.circle(radius)
-    turtle.left(135)  # takes same time, draws pattern differently and leaves turtle in same dirn as it started
+    right(135)  # turtle.right(135)
+    circle(radius)  # turtle.circle(radius)
+    left(135)  # turtle.left(135)  # takes same time, draws pattern differently and leaves turtle in same dirn as it started
     print(f"Inside function, namespace is: {dir()}")  # gives names in a list
     print(f"locals: {locals()}")  # shows the namespace dict, i.e. values too
 
 
 # encircled_square(300)
-# turtle.speed('fast')
-# for s in range(72):
-#     encircled_square(120)
-#     turtle.left(5)
-#
-# turtle.done()
+speed('fast')  # turtle.speed('fast')
+for s in range(72):
+    encircled_square(120)
+    left(5)  # turtle.left(5)
+
+done()  # turtle.done()
 
 print(dir())  # output is a list containing all the names that exist in our namespace, sorted alphabetically, `__` pronounced as dunder
 
