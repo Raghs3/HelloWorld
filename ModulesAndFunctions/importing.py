30675c7ca18fb18a6dad8f81ccf3189605731d34
Raghs3 <tinku.minku2803@gmail.com>
2024-11-23 17:52:51 +0530

    Day 6 - why do you copy globals

--- ModulesAndFunctions/importing.py
+++ ModulesAndFunctions/importing.py
@@ -4,7 +4,7 @@ from better_code import area_of_square
 area = better_code.area_of_square(40)  # area_of_square doesn't exist in global namespace
 area = area_of_square(40)  # only namespace is affected in different ways of importing
 print(area)
-
+# line 5 is faster as only need to go through one namespace
 print('Global namespace')
 namespace = globals().copy()  # if you don't use copy, error is generated
 for name, obj in namespace.items():
