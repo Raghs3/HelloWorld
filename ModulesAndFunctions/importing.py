e1fb46c09aba7a6838503fc6052b6fad3bd1d0c0
Raghs3 <tinku.minku2803@gmail.com>
2024-11-23 17:46:30 +0530

    Day 6 - importing

---
+++ ModulesAndFunctions/importing.py
@@ -0,0 +1,11 @@
+import better_code
+from better_code import area_of_square
+
+area = better_code.area_of_square(40)  # area_of_square doesn't exist in global namespace
+area = area_of_square(40)  # only namespace is affected in different ways of importing
+print(area)
+
+print('Global namespace')
+namespace = globals().copy()
+for name, obj in namespace.items():
+    print(name, obj)
