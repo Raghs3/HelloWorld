ac057e636f0013cac9d44f892be8e589a8f10df3
Raghs3 <tinku.minku2803@gmail.com>
2024-11-25 12:11:15 +0530

    Day 8 - datetime module

--- ModulesAndFunctions/date_example.py
+++ ModulesAndFunctions/date_example.py
@@ -5,3 +5,13 @@ print(start)
 
 pretty_start = start.strftime('%A %d %B, %Y')  # %A is name of day, %d is day, %B is month name, %Y is year
 print(pretty_start)
+
+year = start.year
+month = start.month
+day = start.day
+
+print(f'The {year} winter olympics started on day {day} of month {month}')
+
+today = datetime.date.today()
+print(today)
+print(today.strftime('%A'))
