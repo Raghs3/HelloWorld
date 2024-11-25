793101091d5c619fd1e5522c2c537de939d943b9
Raghs3 <tinku.minku2803@gmail.com>
2024-11-25 15:15:35 +0530

    Day 8 - datetime module

--- ModulesAndFunctions/datetime_example.py
+++ ModulesAndFunctions/datetime_example.py
@@ -1,12 +1,15 @@
 import datetime as dt
 from datetime import datetime  # gives error as it replaces entry by line1, as namespace now contains class not module
 # if we want to use both imports, gotta use aliasing
-today = dt.datetime.today()
+# today = dt.datetime.today()  # might get deprecated soon
 now = datetime.now()
 # utc_now = datetime.utcnow()  # won't work for me coz its deprecated f
-print(today)
+# print(today)
 print(now)  # both give same value
 # print(utc_now)
 # better precision with now if C gettimeofday function supported
 # now can accept optional tzinfo object
 # now is preferred over today
+
+# a time is aware if it contains timezone information
+# otherwise it is naive
