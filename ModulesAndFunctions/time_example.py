4c6ebf02f3f72cf99c4d97f704da967bdaa5e99a
Raghs3 <tinku.minku2803@gmail.com>
2024-11-25 14:59:46 +0530

    Day 8 - datetime module

--- ModulesAndFunctions/time_example.py
+++ ModulesAndFunctions/time_example.py
@@ -8,4 +8,12 @@ print(end_time)
 # can't subtract or use timedelta with time class
 # can't track date
 
-print(end_time - meeting)  # __sub__ not defined for time class
+# print(end_time - meeting)  # __sub__ not defined for time class
+
+iso_time = 'T11:15:00'  # in older version of python, adding `T` gives error
+_time = time.fromisoformat(iso_time)  # used underscore to avoid conflict with time class
+print(_time)
+
+iso_date = '2022-05-10'
+_date = date.fromisoformat(iso_date)
+print(_date)
