b690cf593fb1df0f8341ef6e4fd6046b4ad60d48
Raghs3 <tinku.minku2803@gmail.com>
2024-12-04 19:11:16 +0530

    Day 17 - functions

--- functions/functions.py
+++ functions/functions.py
@@ -1,6 +1,18 @@
 def python_food():
-    print("spam and eggs")
+    width = 80
+    text = "Spam and eggs"
+    left_margin = (width - len(text)) // 2
+    print(" " * left_margin, text)
+    # print("spam and eggs")
+
+
+def centre_text(text):
+    left_margin = (80 - len(text)) // 2
+    print(" " * left_margin, text)
 
 
 # call the function
-python_food()
+# print(python_food())  # all functions return something, if nothing specified it returns None
+centre_text("spam and eggs")
+centre_text("spam, spam and eggs")
+centre_text("spam, spam, spam, and spam")
