01e625cd60848e466ba0c6244a906c16e551e407
Raghs3 <tinku.minku2803@gmail.com>
2024-12-10 19:48:17 +0530

    Day 22 - MoreFunctions

--- MoreFunctions/morefunct.py
+++ MoreFunctions/morefunct.py
@@ -11,8 +11,12 @@ def draw_axes(canvas):
     x_origin = canvas.winfo_width() / 2
     y_origin = canvas.winfo_height() / 2
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="white")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="white")
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+
+
+def plot(canvas, x, y):
+    canvas.create_line(x, y, x + 1, y + 1, fill="red")
 
 
 mainWindow = tkinter.Tk()
@@ -27,6 +31,6 @@ draw_axes(canvas)
 
 for x in range(-100, 100):
     y = parabola(x)
-    print(y)
+    plot(canvas, x, y)
 
 mainWindow.mainloop()
