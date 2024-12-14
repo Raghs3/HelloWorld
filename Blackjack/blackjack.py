7c829e0b4f920b176883553cdea96f673bf3b2a4
Raghs3 <tinku.minku2803@gmail.com>
2024-12-14 10:58:37 +0530

    Day 25 - blackjack

---
+++ Blackjack/blackjack.py
@@ -0,0 +1,17 @@
+import random
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
+
+mainWindow = tkinter.Tk()
+
+# Set up the screen and frames for the dealer and player
+mainWindow.title("Blackjack")
+mainWindow.geometry("640x480")
+
+result_text = tkinter.StringVar()
+result = tkinter.Label(mainWindow, textvariable=result_text)
+
+card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1)
