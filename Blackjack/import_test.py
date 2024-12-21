9f21f02eea18bf41a0716742bdcaf2084a9d3089
Raghs3 <tinku.minku2803@gmail.com>
2024-12-21 13:58:41 +0530

    Day 34 - modules

--- Blackjack/import_test.py
+++ Blackjack/import_test.py
@@ -1,5 +1,21 @@
 import blackjack
 
-print(__name__)
-blackjack.play()  # gives error as no code is executed
-print(blackjack.cards)
+# g = sorted(globals())
+# for x in g:
+#     print(x)
+
+# _deal_card(dealer_card_frame)  # python automatically doesn't import protected methods (when did import *)
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# blackjack.play()
+
+# print(__name__)
+#
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# blackjack.play()
+# print(blackjack.cards)
+
+personal_details = ("Tim", 24, "Australia")
+
+name, _, country = personal_details
+print(name, country)
+print(_)
