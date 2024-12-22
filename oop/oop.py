2f6643dbd6b2b13640d4decf8b8ad5d512cba17a
Raghs3 <tinku.minku2803@gmail.com>
2024-12-22 16:49:54 +0530

    Day 36 - initial coomit

--- oop/oop.py
+++ oop/oop.py
@@ -14,5 +14,7 @@ kenwood.price = 12.75
 print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
-
+# when variable bound to instance of class it is referred to as data attribute
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
