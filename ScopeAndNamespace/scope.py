ccc17c3a3ba3682693af3b9e6fd6d53c6c72ee58
Raghs3 <tinku.minku2803@gmail.com>
2024-12-22 14:11:58 +0530

    Day 36 - Recursion

--- ScopeAndNamespace/scope.py
+++ ScopeAndNamespace/scope.py
@@ -2,7 +2,7 @@ def fact(n):
     """ calculate n! iteratively"""
     result = 1
     if n > 1:
-        for f in range(2, n+1):
+        for f in range(2, n + 1):
             result *= f
     return result
 
@@ -16,5 +16,28 @@ def factorial(n):
         return n * factorial(n-1)
 
 
-for i in range(130):
-    print(i, factorial(i))
+def fib(n):  # inefficient
+    """ F(n) = F(n-1) + F(n-2)"""
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def fibonacci(n):  # faster
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minus1 = 1
+        n_minus2 = 0
+        for f in range(1, n):
+            result = n_minus2 + n_minus1
+            n_minus2 = n_minus1
+            n_minus1 = result
+    return result
+
+
+for i in range(36):
+    print(i, fibonacci(i))
