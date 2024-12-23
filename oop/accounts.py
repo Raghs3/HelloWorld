1df29d05fc851ab322486713366b6246a579fba4
Raghs3 <tinku.minku2803@gmail.com>
2024-12-23 13:26:03 +0530

    Day 36 - static method

--- oop/accounts.py
+++ oop/accounts.py
@@ -1,9 +1,13 @@
 import datetime
-import pytz
 
 
 class Account:
     """Simple account class with balance"""
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.now(datetime.UTC)
+        return utc_time
 
     def __init__(self, name, balance):  # technically constructor is __new__
         self.name = name
@@ -15,11 +19,13 @@ class Account:
         if amount > 0:
             self.balance += amount
             self.show_balance()
-            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+            # self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+            self.transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))
         else:
             print("The amount must be greater than zero and no more than your account balance")
         self.show_balance()
@@ -33,7 +39,9 @@ class Account:
                 tran_type = "deposited"
             else:
                 tran_type = "withdrawn"
-            print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
+                amount *= -1
+            # print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
+            print(f"{amount:6} {tran_type} on {date} (local time was {date.astimezone()})")
 
 
 if __name__ == '__main__':
@@ -46,3 +54,5 @@ if __name__ == '__main__':
     # raghs3.show_balance()
 
     raghs3.withdraw(10000)
+
+    raghs3.show_transaction()
