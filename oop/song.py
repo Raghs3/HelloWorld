96626c63455af8aa910204b0e2e7c2d6605b57a0
Raghs3 <tinku.minku2803@gmail.com>
2024-12-23 17:02:33 +0530

    Day 36 - load data and checkfile

--- oop/song.py
+++ oop/song.py
@@ -123,6 +123,19 @@ def load_data():
     return artist_list
 
 
+def create_checkfile(artist_list):
+    """Create a check file from the object data for comparision with the original file
+    """
+    with open("checkfile.txt", "w") as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.tracks:
+                    print(f"{new_artist.name}\t{new_album.name}\t{new_album.year}\t{new_song.title}",
+                          file=checkfile)
+
+
 if __name__ == '__main__':
     artists = load_data()
     print(f"There are {len(artists)} artists")
+
+    create_checkfile(artists)
