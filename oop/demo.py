432c9b89b9cfd3806f15f86e46ac16bc7ff96e0e
Raghs3 <tinku.minku2803@gmail.com>
2024-12-23 14:12:57 +0530

    Day 36 - raw string (literal)

--- oop/demo.py
+++ oop/demo.py
@@ -6,3 +6,12 @@ print(raw_string)
 
 b_string = "this is" + chr(10) + "a string split" + chr(9) + "and tabbed"
 print(b_string)
+
+backslash_string = "this is a backslash \followed by some text"
+print(backslash_string)
+
+backslash_string = "this is a backslash \\followed by some text"
+print(backslash_string)
+
+error_string = r"this string ends with \\"  # escapes final qoute so need the extra backslash
+print(error_string)
