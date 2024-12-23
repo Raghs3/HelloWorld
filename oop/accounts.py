51ad81d88dc88eb4528639fc1679e8b2f8ab48d0
Raghs3 <tinku.minku2803@gmail.com>
2024-12-23 13:53:05 +0530

    Day 36 - non public and mangling

--- oop/accounts.py
+++ oop/accounts.py
@@ -10,31 +10,32 @@ class Account:
         return utc_time
 
     def __init__(self, name, balance):  # technically constructor is __new__
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for " + self.name)
+        self._name = name
+        self.__balance = balance  # mangling, won't be done if ends with two underscores
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
             # self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            self._transaction_list.append((Account._current_time(), -amount))
         else:
             print("The amount must be greater than zero and no more than your account balance")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
     def show_transaction(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = "deposited"
             else:
@@ -56,3 +57,13 @@ if __name__ == '__main__':
     raghs3.withdraw(10000)
 
     raghs3.show_transaction()
+
+    steph = Account("Steph", 800)
+    steph.__balance = 200  # don't get warning but we should remember the convention
+    steph.deposit(100)
+    steph.withdraw(200)
+    steph.show_transaction()
+    steph.show_balance()  # balance value not changed even though assigned a new value because of mangling
+    print(steph.__dict__)  # actual attribute is now renamed as _Account__balance so it is hidden and prevents accidental change
+    steph._Account__balance = 40  # discouraged
+    steph.show_balance()  # now it works
