d557a2865b0b4c627768b01e4ca8498863ba4819
Raghs3 <tinku.minku2803@gmail.com>
2024-12-23 16:14:24 +0530

    Day 36 - album class

--- oop/song.py
+++ oop/song.py
@@ -13,15 +13,55 @@ class Song:
         self.duration = duration
 
 
-# help(Song.__init__)
-print(Song.__doc__)
-print(Song.__init__.__doc__)
-Song.__init__.__doc__ = """ Song init method
+class Album:
+    """Class to represent an Album, using it's track list
+
+    Attributes:
+        album_name (str): The name of the album.
+        year (int): The year album was released.
+        artist (Artist): The artist responsible for the album. If not specified,
+        the artist will default to an artist with the name "Varius Artists".
+        tracks (List[Song]): A list of the songs on the album.
+
+    Methods:
+        add_song: Used to add a new song to the album's track list.
+    """
+
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various Artists")
+        else:
+            self.artist = artist
+
+        self.tracks = []
+
+    def add_song(self, song, position=None):
+        """Adds a song to the track list
 
         Args:
-            title (str):  Initialises the 'title' attribute.
-            artist (Artist): An Artist object representing the song's creator.
-            duration (Optional [int]): Initial value for the 'duration' attribute.
-                Will default to zero if not specified.
+            song (Song): A song to add.
+            position (Optional[int]): If specified, the song will be added to that position
+                in the track list - inserting it between other songs if necessary.
+                Otherwise, the song will be added to the end of the list.
         """
-help(Song)
+
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position, song)
+
+
+# # help(Song.__init__)
+# print(Song.__doc__)
+# print(Song.__init__.__doc__)
+# Song.__init__.__doc__ = """ Song init method
+#
+#         Args:
+#             title (str):  Initialises the 'title' attribute.
+#             artist (Artist): An Artist object representing the song's creator.
+#             duration (Optional [int]): Initial value for the 'duration' attribute.
+#                 Will default to zero if not specified.
+#         """
+# help(Song)
