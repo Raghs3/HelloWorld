ceb4563eb45312016c12b7614f7aa2d32c607835
Raghs3 <tinku.minku2803@gmail.com>
2025-01-09 08:54:39 +0530

    Day 52 - getters and setters

--- Game/main.py
+++ Game/main.py
@@ -6,3 +6,6 @@ print(tim.name)
 print(tim.lives)
 tim.lives -= 1
 print(tim.lives)
+
+# print(tim.get_name())  # get_name method is called as a getter
+# tim.set_lives(300)  # setter
--- Game/player.py
+++ Game/player.py
@@ -2,6 +2,14 @@ class Player(object):
 
     def __init__(self, name):
         self.name = name
-        self.lives = 3
+        self._lives = 3
         self.level = 1
         self.score = 0
+
+    def _get_lives(self):
+        return self._lives
+
+    def _set_lives(self, lives):
+        self._lives = lives
+
+    lives = property(_get_lives, _set_lives)
