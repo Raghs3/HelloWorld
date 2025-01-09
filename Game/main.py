fc4ebd5dcc18e587ee4eda1be81a258d405234b6
Raghs3 <tinku.minku2803@gmail.com>
2025-01-09 08:47:20 +0530

    Day 52 - getters and setters

--- Game/main.py
+++ Game/main.py
@@ -1,6 +1,8 @@
-import player
+from player import Player
 
-tim = player.Player("Tim")
+tim = Player("Tim")  # tim = player.Player("Tim")
 
 print(tim.name)
+print(tim.lives)
+tim.lives -= 1
 print(tim.lives)
