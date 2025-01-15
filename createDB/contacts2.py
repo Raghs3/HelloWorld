889139b316e25f4153de5f91ff20c468264c873c
Raghs3 <tinku.minku2803@gmail.com>
2025-01-15 12:43:18 +0530

    Day 59 - Database SQLite in Python

--- createDB/contacts2.py
+++ createDB/contacts2.py
@@ -1,6 +1,13 @@
 import sqlite3
+from itertools import count
+
 # sqlite wraps insertions and deletions in transaction so entire transaction can be roll backed
 db = sqlite3.connect("contacts.sqlite")
+
+update_sql = "UPDATE contacts SET email = 'update@update.com'"  # WHERE contacts.phone = 4567"
+update_cursor = db.cursor()
+update_cursor.execute(update_sql)
+print(f"{update_cursor.rowcount} rows updated")
 
 for name, phone, email in db.execute("SELECT * FROM contacts"):  # shortcut instead of cursor
     print(name)  # all data inserted is rolled back when db closed unless committed
