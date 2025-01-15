87535f08977079d03574dda51719d35a9024330a
Raghs3 <tinku.minku2803@gmail.com>
2025-01-15 12:36:36 +0530

    Day 59 - Database SQLite in Python

--- createDB/contacts2.py
+++ createDB/contacts2.py
@@ -2,7 +2,10 @@ import sqlite3
 # sqlite wraps insertions and deletions in transaction so entire transaction can be roll backed
 db = sqlite3.connect("contacts.sqlite")
 
-for row in db.execute("SELECT * FROM contacts"):  # all data inserted is rolled back when db closed
-    print(row)
-# prints nothing as there is nothing in the db as we didn't commit when inserting data in previous script
+for name, phone, email in db.execute("SELECT * FROM contacts"):  # shortcut instead of cursor
+    print(name)  # all data inserted is rolled back when db closed unless committed
+    print(phone)
+    print(email)
+    print('-' * 20)
+# prints nothing as there is nothing in the db as we didn't commit when inserting data in previous script (now committed)
 db.close()
