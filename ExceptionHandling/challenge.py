5c881feacd1e101de34cf5dd2215fb6fa9727211
Raghs3 <tinku.minku2803@gmail.com>
2025-01-25 23:50:46 +0530

    Day 67 - Exceptions (Error handling)

---
+++ ExceptionHandling/challenge.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number "))
+b = int(input("Enter second number "))
+
+try:
+    print(a/b)
+except:
+    print("Are you sure you entered an integer? or the second number is not a zero?")
