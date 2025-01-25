aded76db6ed3e96580553be0346fd6a43f17c788
Raghs3 <tinku.minku2803@gmail.com>
2025-01-26 00:03:51 +0530

    Day 67 - Exceptions (Error handling)

--- ExceptionHandling/challenge.py
+++
@@ -1,8 +0,0 @@
-# my soln
-a = int(input("Enter a number "))
-b = int(input("Enter second number "))
-
-try:
-    print(a/b)
-except (TypeError, ZeroDivisionError):
-    print("Are you sure you entered an integer? or the second number is not a zero?")
---
+++ ExceptionHandling/get_ints(challenge).py
@@ -0,0 +1,28 @@
+# # my soln
+# a = int(input("Enter a number "))
+# b = int(input("Enter second number "))
+# # my error: didn't check if user entered numbers or not
+# try:
+#     print(a/b)
+# except (TypeError, ZeroDivisionError):
+#     print("Are you sure you entered an integer? or the second number is not a zero?")
+
+
+# tim soln
+def getint(prompt):
+    while True:
+        try:
+            number = int(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid number entered, please try again")
+
+
+# x = int(input("Enter a number "))  # to check what is the exception thrown
+first_num = getint("Please enter first number ")
+second_num = getint("Please enter second number ")
+
+try:
+    print(f"{first_num} divided by {second_num} is {first_num/second_num}")
+except ZeroDivisionError:
+    print("You can't divide by zero")
