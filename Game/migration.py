7914fdc47bfd521bc88738eda693d0c43cf3a8b0
Raghs3 <tinku.minku2803@gmail.com>
2025-02-06 23:28:31 +0530

    Day 81 - raising errors

--- Game/migration.py
+++ Game/migration.py
@@ -8,12 +8,14 @@ duck4 = ducks.Duck()
 duck5 = ducks.Duck()
 duck6 = ducks.Duck()
 duck7 = ducks.Duck()
+percy = ducks.Penguin()
 
 flock.add_duck(donald)
 flock.add_duck(daisy)
 flock.add_duck(duck3)
 flock.add_duck(duck4)
-flock.add_duck(duck5)
+flock.add_duck(percy)  # error as penguin object has no attribute
+flock.add_duck(duck5)  # other 3 remained grounded, unable to fly, only 4 ducks fly
 flock.add_duck(duck6)
 flock.add_duck(duck7)
 
