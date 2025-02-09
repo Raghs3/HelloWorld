b5047183d6e1621d0d3231bc09a90029d4ba9065
Raghs3 <tinku.minku2803@gmail.com>
2025-02-09 23:32:43 +0530

    Day 84 - raising errors

--- Game/migration.py
+++ Game/migration.py
@@ -17,6 +17,6 @@ flock.add_duck(duck4)
 flock.add_duck(percy)  # error as penguin object has no attribute fly
 flock.add_duck(duck5)  # other 3 remained grounded, unable to fly, only 4 ducks fly
 flock.add_duck(duck6)  # error is shown in different places, gets passed to call stack until something handles it or reaches runtime
-flock.add_duck(duck7)
+flock.add_duck(duck7)  # stack trace and output can be mixed up and hence output can't always be trusted
 
 flock.migrate()
