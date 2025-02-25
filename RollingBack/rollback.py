44df546a724ee953de77d0d59913b363339c090e
Raghs3 <tinku.minku2803@gmail.com>
2025-02-25 21:30:22 +0530

    Day 99 - rolling back transacgtions (back to databases)

--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -2,6 +2,12 @@ class Account(object):
 
     def __init__(self, name: str, opening_balance: float = 0.0):
         self.name = name
-        self.balance = opening_balance
-        print(f"Account created for {self.name}", end="")
+        self._balance = opening_balance
+        print(f"Account created for {self.name}", end='')
         self.show_balance()
+
+    def deposit(self, amount: float) -> float:
+        if amount > 0.0:
+            self._balance += amount
+            print(f"{amount} deposited")
+        return self._balance
