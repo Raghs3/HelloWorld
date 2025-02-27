98a8e40f6b0e88ea6d91e8a0e5251c8f375f43b2
Raghs3 <tinku.minku2803@gmail.com>
2025-02-26 17:46:16 -1100

    Day 100 - rolling back transacgtions (back to databases)

--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -11,3 +11,12 @@ class Account(object):
             self._balance += amount
             print(f"{amount} deposited")
         return self._balance
+
+    def withdraw(self, amount: float) -> float:
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            print(f"{amount} withdrawn")
+            return amount
+        else:
+            print("The amount must be greater than zero and no more than your account balance")
+            return 0.0
