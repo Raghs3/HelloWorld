73d9e0292de8a3f1bc6bbbd24b308bdb000ba2f3
Raghav Kumar <tinku.minku2803@gmail.com>
2025-03-06 23:38:46 +0530

    Day 109 - rolling back transactions (back to databases)

--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -1,35 +1,35 @@
 class Account(object):
 
-    def __init__(self, name: str, opening_balance: float = 0.0):
+    def __init__(self, name: str, opening_balance: int = 0):
         self.name = name
         self._balance = opening_balance
         print(f"Account created for {self.name}", end='')
         self.show_balance()
 
-    def deposit(self, amount: float) -> float:
+    def deposit(self, amount: int) -> float:
         if amount > 0.0:
             self._balance += amount
-            print(f"{amount} deposited")
-        return self._balance
+            print(f"{amount/100:.2f} deposited")
+        return self._balance / 100
 
-    def withdraw(self, amount: float) -> float:
+    def withdraw(self, amount: int) -> float:
         if 0 < amount <= self._balance:
             self._balance -= amount
-            print(f"{amount} withdrawn")
-            return amount
+            print(f"{amount/100:.2f} withdrawn")  # working in cents and pennies instead of dollars or pounds
+            return amount / 100
         else:
             print("The amount must be greater than zero and no more than your account balance")
             return 0.0
 
     def show_balance(self):
-        print(f"Balance on account {self.name} is {self._balance}")
+        print(f"Balance on account {self.name} is {self._balance/100:.2f}")
 
 
 if __name__ == "__main__":
     john = Account("John")
-    john.deposit(10.10)
-    john.deposit(0.10)
-    john.deposit(0.10)
-    john.withdraw(0.30)  # when working with float we encounter rounding errors and so exact value doesn't show (instead of 10 we get 9.999998)
+    john.deposit(1010)  # now using integers instead of float (pennies and cents)
+    john.deposit(10)
+    john.deposit(10)
+    john.withdraw(30)  # when working with float we encounter rounding errors and so exact value doesn't show (instead of 10 we get 9.999998)
     john.withdraw(0)  # recurring or some types of issue, lose accuracy with every 0.1
     john.show_balance()  # we can use decimal class (not teaching here) (code in rollback2 though won't understand)
