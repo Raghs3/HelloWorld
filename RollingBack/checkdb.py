60df98888522c2fcd3ab589a53d5c4388f8dee8a
Raghav Kumar <tinku.minku2803@gmail.com>
2025-04-01 10:03:36 +0530

    Day 135 - rolling back transactions (back to databases) (gui database)

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -3,7 +3,7 @@ import sqlite3
 
 db = sqlite3.connect("accounts.sqlite")
 
-for row in db.execute("SELECT * FROM history"):
+for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
     # print(row)
     local_time = row[0]
     print(f"{local_time}\t{type(local_time)}")  # time is in string datatype
