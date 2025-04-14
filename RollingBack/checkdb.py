d5f1466f261272fcdf108f3d4c821d9dc120af2a
Raghs3 <tinku.minku2803@gmail.com>
2025-04-15 02:04:08 +0530

    Day 149 - checking if everything works on new laptop

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -1,12 +1,13 @@
 import sqlite3
 import datetime
+import pytz
 
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
 
 for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
     utc_time = row[0]
-    local_time = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    local_time = pytz.utc.localize(utc_time).astimezone()
     # print(row)
     print(f"{utc_time}\t{local_time}")  # time is in string datatype
 
