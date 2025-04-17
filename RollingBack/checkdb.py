92920610dd2579585925843d82908d396dc9544f
Raghs3 <tinku.minku2803@gmail.com>
2025-04-17 22:02:37 +0530

    Day 151 - timezones in database

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -7,7 +7,7 @@ db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  #
 
 for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
     utc_time = row[0]
-    local_time = utc_time.astimezone()
+    local_time = utc_time.astimezone()  # why not giving in my timezone!! this is the soln pretty sure ;-;
     # print(row)
     print(f"{utc_time}\t{local_time}")  # time is in string datatype
 
