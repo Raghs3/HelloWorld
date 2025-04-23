37fd40590559238f4bf43d5f7336e99ad4fb63cc
Raghs3 <tinku.minku2803@gmail.com>
2025-04-23 20:46:08 +0530

    Day 157 - something about ISO i got from the qna, gotta do more research

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -3,7 +3,7 @@ import datetime
 import pytz
 
 
-db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
+db = sqlite3.connect("accounts_iso.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
 
 for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
     utc_time = row[0]
