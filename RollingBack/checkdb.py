89ac97b3aeb1c318ee3fe697bd1df94f46c5d937
Raghs3 <tinku.minku2803@gmail.com>
2025-04-25 21:34:30 +0530

    Day 159 - so I have done some qnaing in my course, and now it works

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -1,15 +1,32 @@
 import sqlite3
 import datetime
-import pytz
+# import pytz
+from SQLite_Converters import adapt_datetime_iso  # , convert_datetime # if writing to the database
+
+sqlite3.register_adapter(datetime, adapt_datetime_iso)
+# sqlite3.register_converter("datetime", convert_datetime)  # only needed if writing.
 
 
-db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
+# tim old
+# db = sqlite3.connect("accounts_iso.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
+#
+# for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
+#     utc_time = row[0]
+#     # local_time = utc_time.astimezone()  # why not giving in my timezone!! this is the soln pretty sure ;-;
+#     # print(row)
+#     local_time = pytz.utc.localize(utc_time).astimezone()  # i'll just use pytz and move on for now ;-;
+#     print(f"{utc_time}\t{local_time}")  # time is in string datatype
+#
+# db.close()  # remember to close the connection!
+
+
+# my new
+db = sqlite3.connect("accounts_iso.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
 
 for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
     utc_time = row[0]
-    # local_time = utc_time.astimezone()  # why not giving in my timezone!! this is the soln pretty sure ;-;
+    local_time = utc_time.astimezone()  # why not giving in my timezone!! this is the soln pretty sure ;-;
     # print(row)
-    local_time = pytz.utc.localize(utc_time).astimezone()  # i'll just use pytz and move on for now ;-;
     print(f"{utc_time}\t{local_time}")  # time is in string datatype
 
 db.close()  # remember to close the connection!
