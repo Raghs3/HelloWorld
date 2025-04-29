c259e57aa6fb75db7c327fd8fa726a353d236a7c
Raghs3 <tinku.minku2803@gmail.com>
2025-04-29 10:14:12 +0530

    Day 162 - datetime in databases

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -1,12 +1,11 @@
 import sqlite3
-import pytz
+# import pytz
 
 # tim old
-db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so item is datetime.datetime
+db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)  # using detect_types makes it so the item is datetime.datetime
 
-for row in db.execute("SELECT * FROM history"):  # doing this we realised our time is in str
-    utc_time = row[0]
-    local_time = pytz.utc.localize(utc_time).astimezone()  # i'll just use pytz and move on for now ;-;
-    print(f"{utc_time}\t{local_time}")  # time is in string datatype
+for row in db.execute("SELECT strftime('%d-%m-%Y %H:%M:%f', history.time, 'localtime') AS localtime,"
+                      " history.account, history.amount FROM history ORDER BY history.time"):  # ("SELECT * FROM history"): # doing this we realised our time is in str
+    print(row)
 
 db.close()  # remember to close the connection!
