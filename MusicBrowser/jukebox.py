0afeb5d83ce3f77c9439e37617f098d8de7f7a03
Raghs3 <tinku.minku2803@gmail.com>
2025-05-26 09:59:08 +0530

    Day 190 - UI for DB (jukebox)

--- MusicBrowser/jukebox.py
+++ MusicBrowser/jukebox.py
@@ -5,3 +5,17 @@ except ImportError:  # python 2
     import Tkinter as tkinter
 
 conn = sqlite3.connect('music.db')
+
+mainWindow = tkinter.Tk()
+mainWindow.title("Music DB Browser")
+mainWindow.geometry('1024x768')
+
+mainWindow.columnconfigure(0, weight=2)
+mainWindow.columnconfigure(1, weight=2)
+mainWindow.columnconfigure(2, weight=2)
+mainWindow.columnconfigure(3, weight=1)  # spacer column on right
+
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=5)
+mainWindow.rowconfigure(2, weight=5)
+mainWindow.rowconfigure(3, weight=1)
