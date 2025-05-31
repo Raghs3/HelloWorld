fa9b745ec2f50990bdd1645bde58ca747cfe2f4a
Raghs3 <tinku.minku2803@gmail.com>
2025-05-31 09:42:52 +0530

    Day 195 - star args

--- MusicBrowser/star_args.py
+++ MusicBrowser/star_args.py
@@ -1,1 +1,10 @@
-print("Hello", "world")  # values are wrapped in tuple, *args is unpacked values in tuple
+# print("Hello", "planet", "earth")  # values are wrapped in tuple, *args is unpacked values in tuple
+
+
+def average(*args):
+    mean = 0
+    for arg in args:
+        mean += arg
+    return mean / len(args)
+
+print(average(1, 2, 3, 4))
