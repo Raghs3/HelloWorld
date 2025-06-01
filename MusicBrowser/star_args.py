10d9bc3c72bafefb3c830c648bb68ede4cff036d
Raghs3 <tinku.minku2803@gmail.com>
2025-06-01 09:43:29 +0530

    Day 196 - star args

--- MusicBrowser/star_args.py
+++ MusicBrowser/star_args.py
@@ -5,6 +5,6 @@ def average(*args):
     mean = 0
     for arg in args:
         mean += arg
-    return mean / len(args)
+    return mean / len(args)  # in python2 it does integer division
 
 print(average(1, 2, 3, 4))
