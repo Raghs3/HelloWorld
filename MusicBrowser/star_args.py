65fe0cb71ad970edd71f4fa5dd4f1f384c4d1504
Raghs3 <tinku.minku2803@gmail.com>
2025-06-06 11:42:36 +0530

    Day 200 - star args

--- MusicBrowser/star_args.py
+++ MusicBrowser/star_args.py
@@ -15,3 +15,13 @@ print(average(1, 2, 3, 4))
 
 def build_tuple(*args):  # challenge
     return args
+
+
+#test
+message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")
+print(type(message_tuple))
+print(message_tuple)
+
+number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+print(type(number_tuple))
+print(number_tuple)
