5906196c87a25d9eb155edd078a6f9ba1605ba89
Raghs3 <tinku.minku2803@gmail.com>
2025-06-25 15:50:14 +0530

    Day 219 - populating tkinter listbox from database

--- MusicBrowser/jukebox.py
+++ MusicBrowser/jukebox.py
@@ -4,7 +4,7 @@ try:
 except ImportError:  # python 2
     import Tkinter as tkinter
 
-conn = sqlite3.connect('music.sqlite')
+# conn = sqlite3.connect('music.sqlite')
 
 
 class Scrollbox(tkinter.Listbox):
@@ -86,86 +86,88 @@ class DataListBox(Scrollbox):
         # albumLV.set(tuple(alist))
         # songLV.set(("Choose an album",))
 
-def get_songs(event):  # don't want to do this as duplication
-    lb = event.widget
-    index = int(lb.curselection()[0])
-    album_name = lb.get(index),
-
-    # get the artist ID grom the database row
-    album_id = conn.execute("SELECT albums._id FROM albums WHERE albums.name=?", album_name).fetchone()
-    alist = []
-    for x in conn.execute("SELECT songs.title FROM songs WHERE songs.album=? ORDER BY songs.track", album_id):
-        alist.append(x[0])
-    songLV.set(tuple(alist))
-
-
-mainWindow = tkinter.Tk()
-mainWindow.title("Music DB Browser")
-mainWindow.geometry('1024x768')
-
-mainWindow.columnconfigure(0, weight=2)
-mainWindow.columnconfigure(1, weight=2)
-mainWindow.columnconfigure(2, weight=2)
-mainWindow.columnconfigure(3, weight=1)  # spacer column on right
-
-mainWindow.rowconfigure(0, weight=1)
-mainWindow.rowconfigure(1, weight=5)
-mainWindow.rowconfigure(2, weight=5)
-mainWindow.rowconfigure(3, weight=1)
-
-# ===== labels =====
-tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
-tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
-tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
-
-# ===== Artists Listbox =====
-# artistList = Scrollbox(mainWindow)
-artistList = DataListBox(mainWindow, conn, "artists", "name")
-artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30,0))
-artistList.config(border=2, relief='sunken')
-# don't need to create scrollbar, as it displays its own scrollbar that we did in class
-# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
-# artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
-# artistList['yscrollcommand'] = artistScroll.set  # does the communication between listbox and scroll bar
-
-# for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
-#     artistList.insert(tkinter.END, artist[0])
+# def get_songs(event):  # don't want to do this as duplication
+#     lb = event.widget
+#     index = int(lb.curselection()[0])
+#     album_name = lb.get(index),
 #
+#     # get the artist ID grom the database row
+#     album_id = conn.execute("SELECT albums._id FROM albums WHERE albums.name=?", album_name).fetchone()
+#     alist = []
+#     for x in conn.execute("SELECT songs.title FROM songs WHERE songs.album=? ORDER BY songs.track", album_id):
+#         alist.append(x[0])
+#     songLV.set(tuple(alist))
 
-artistList.requery()
-# artistList.bind('<<ListboxSelect>>', get_albums)  # now in class init function
+if __name__ == '__main__':
+    conn = sqlite3.connect('music.sqlite')
 
-# ===== Albums Listbox =====
-albumLV = tkinter.Variable(mainWindow)
-albumLV.set(("Choose an artist",))
-# albumList = Scrollbox(mainWindow, listvariable=albumLV)
-albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))
-albumList.requery(12)
-albumList.grid(row=1, column=1, sticky='nsew', padx=(30,0))
-albumList.config(border=2, relief='sunken')
-# going to see how to create scrollable list box class to avoid duplication  # see class, hence don't need bottom lines
-# albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
-# albumScroll.grid(row=1, column=1, sticky='nse')
-# albumList['yscrollcommand'] = albumScroll.set  # does the communication between listbox and scroll bar
+    mainWindow = tkinter.Tk()
+    mainWindow.title("Music DB Browser")
+    mainWindow.geometry('1024x768')
 
-# albumList.bind('<<ListboxSelect>>', get_songs)
-artistList.link(albumList, "artist")
+    mainWindow.columnconfigure(0, weight=2)
+    mainWindow.columnconfigure(1, weight=2)
+    mainWindow.columnconfigure(2, weight=2)
+    mainWindow.columnconfigure(3, weight=1)  # spacer column on right
 
-# ===== Songs Listbox =====
-songLV = tkinter.Variable(mainWindow)
-songLV.set(("Choose an album",))
-# songList = Scrollbox(mainWindow, listvariable=songLV)
-songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
-songList.requery()
-songList.grid(row=1, column=2, sticky='nsew', padx=(30,0))
-songList.config(border=2, relief='sunken')
+    mainWindow.rowconfigure(0, weight=1)
+    mainWindow.rowconfigure(1, weight=5)
+    mainWindow.rowconfigure(2, weight=5)
+    mainWindow.rowconfigure(3, weight=1)
 
-albumList.link(songList, "album")
+    # ===== labels =====
+    tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
+    tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
+    tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
-# ===== Main loop =====
-testList = range(0, 100)
-albumLV.set(tuple(testList))
-# albumLV.set((1, 2, 3, 4, 5))  # updates the listbox in gui with the values in the tuple
-mainWindow.mainloop()
-print("closing database connection")
-conn.close()
+    # ===== Artists Listbox =====
+    # artistList = Scrollbox(mainWindow)
+    artistList = DataListBox(mainWindow, conn, "artists", "name")
+    artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30,0))
+    artistList.config(border=2, relief='sunken')
+    # don't need to create scrollbar, as it displays its own scrollbar that we did in class
+    # artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
+    # artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
+    # artistList['yscrollcommand'] = artistScroll.set  # does the communication between listbox and scroll bar
+
+    # for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
+    #     artistList.insert(tkinter.END, artist[0])
+    #
+
+    artistList.requery()
+    # artistList.bind('<<ListboxSelect>>', get_albums)  # now in class init function
+
+    # ===== Albums Listbox =====
+    albumLV = tkinter.Variable(mainWindow)
+    albumLV.set(("Choose an artist",))
+    # albumList = Scrollbox(mainWindow, listvariable=albumLV)
+    albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))
+    # albumList.requery(12)
+    albumList.grid(row=1, column=1, sticky='nsew', padx=(30,0))
+    albumList.config(border=2, relief='sunken')
+    # going to see how to create scrollable list box class to avoid duplication  # see class, hence don't need bottom lines
+    # albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
+    # albumScroll.grid(row=1, column=1, sticky='nse')
+    # albumList['yscrollcommand'] = albumScroll.set  # does the communication between listbox and scroll bar
+
+    # albumList.bind('<<ListboxSelect>>', get_songs)
+    artistList.link(albumList, "artist")
+
+    # ===== Songs Listbox =====
+    songLV = tkinter.Variable(mainWindow)
+    songLV.set(("Choose an album",))
+    # songList = Scrollbox(mainWindow, listvariable=songLV)
+    songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
+    # songList.requery()
+    songList.grid(row=1, column=2, sticky='nsew', padx=(30,0))
+    songList.config(border=2, relief='sunken')
+
+    albumList.link(songList, "album")
+
+    # ===== Main loop =====
+    # testList = range(0, 100)
+    # albumLV.set(tuple(testList))
+    # albumLV.set((1, 2, 3, 4, 5))  # updates the listbox in gui with the values in the tuple
+    mainWindow.mainloop()
+    print("closing database connection")
+    conn.close()
