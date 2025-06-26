8ed50f084af8634dcf45a5abdbf1be247ec4540d
Raghs3 <tinku.minku2803@gmail.com>
2025-06-26 15:28:06 +0530

    Day 220 - Generators

---
+++ genexample/fibgen.py
@@ -0,0 +1,33 @@
+def fibonacci():  # infinite generator
+    current, previous = 0, 1
+    while True:
+        yield current
+        current, previous = current + previous, current
+
+
+fib = fibonacci()
+
+# for f in fib:  # don't do this or will end up in infinite loop
+#     print(f)
+
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
