0533ca63069838b76f3f40ec1a297cad550eae8f
Raghs3 <tinku.minku2803@gmail.com>
2025-06-26 14:58:51 +0530

    Day 220 - Generators

--- genexample/size.py
+++ genexample/size.py
@@ -2,21 +2,27 @@ import sys
 
 
 def my_range(n: int):
+    print("my_range starts")
     start = 0
     while start < n:
+        print("my_range is returning {}".format(start))  # function is called 5 times
         yield start  # function finishes after returning so have to use yield instead so further lines can be executed
-        start += 1
+        start += 1  # yielding puts function in suspended state, doesn't actually end it
 
-
+_ = input("line 12")
 # big_range = range(5)  # after changing value of range, size of range still same while size of list 10 times
-big_range = my_range(5)
-
+big_range = my_range(5)  # function isn't actually executed here
+_ = input("line 15")
+# don't need line 14, directly use my_range(5) in for loop, doing line 14 in some cases might even be a bad idea
 print(f"big_range is {sys.getsizeof(big_range)} bytes")
 
 # create a list containing all the values in big_range
 big_list = []
+
+_ = input("line 22")
 for val in big_range:
-    big_list.append(val)
+    _ = input("line 24 - inside loop")
+    big_list.append(val)  # function executed here, while running notice the start value continues on instead of restarting
 
 print(f"big_list is {sys.getsizeof(big_list)} bytes")
 
