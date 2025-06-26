7ed2709f8f4b97de825dd552c4a13035a4d30fa1
Raghs3 <tinku.minku2803@gmail.com>
2025-06-26 22:07:18 +0530

    Day 220 - Generator with computer files

--- musicfiles/filegen.py
+++ musicfiles/filegen.py
@@ -3,9 +3,20 @@ import os
 root = "music"
 
 for path, directories, files in os.walk(root, topdown=True):
-    print(path)
-    print(directories)
-    print(files)
-    input()
+    if files:
+        print(path)
+        first_split = os.path.split(path)
+        print(first_split)
+        second_split = os.path.split(first_split[0])
+        print(second_split)
+        for f in files:
+            song_details = f[:-5].split(' - ')  # STRIP doesn't work as it removes useful characters too, so using slicing
+            print(song_details)
+        print("*" * 40)
+
+
+    # print(directories)
+    # print(files)
+    # input()
     # for f in files:
     #     print("\t{}".format(f))
