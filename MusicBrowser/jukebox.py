d51a026024d7bf96321b01ae22da960d2529c4a2
Raghs3 <tinku.minku2803@gmail.com>
2025-06-26 12:02:51 +0530

    Day 220 - populating tkinter listbox from database

--- MusicBrowser/jukebox.py
+++ MusicBrowser/jukebox.py
@@ -31,6 +31,7 @@ class DataListBox(Scrollbox):
 
         self.linked_box = None
         self.link_field = None
+        self.link_value = None
 
         self.cursor = connection.cursor()
         self.table = table
@@ -52,6 +53,7 @@ class DataListBox(Scrollbox):
         widget.link_field = link_field
 
     def requery(self, link_value=None):
+        self.link_value = link_value  # store the id, so we know the "master" record we're populated from
         if link_value and self.link_field:
             sql = self.sql_select + " WHERE " + self.link_field + "=?" + self.sql_sort
             print(sql)    # TODO delete this line
@@ -75,8 +77,20 @@ class DataListBox(Scrollbox):
             index = self.curselection()[0]
             value = self.get(index),
 
+            # get the ID from the database row
+            # make sure we're getting the correct one, by including the link_value if appropriate
+            if self.link_value:
+                value = value[0], self.link_value
+                sql_where = " WHERE " + self.field + "=? AND " + self.link_field + "=?"
+            else:
+                sql_where = " WHERE " + self.field + "=?"
+
             # get the artist ID from the database row
-            link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]  # may cause errors on other programs using different connection names (not conn)
+            # link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]  # may cause errors on other programs using different connection names (not conn)
+            link_id = self.cursor.execute(self.sql_select + sql_where, value).fetchone()[1]  # may cause errors on other programs using different connection names (not conn)
+
+            # SELECT name, _id FROM albums WHERE name = 'Greatest Hits'  # due to this we get duplicates, as in all ppl have same songs in that album  # fix this
+
             self.linked_box.requery(link_id)
 
         # artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
