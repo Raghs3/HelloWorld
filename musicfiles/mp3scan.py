17187dd531068f4f318860ba5594824625548745
Raghs3 <tinku.minku2803@gmail.com>
2025-06-29 22:45:21 +0530

    Day 223 - Generator with computer files

--- musicfiles/mp3scan.py
+++ musicfiles/mp3scan.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import id3reader_p3 as id3reader
 
 
 def find_music(start, extension):
@@ -9,13 +10,19 @@ def find_music(start, extension):
             yield os.path.join(absolute_path, file)         # use it in yielded values
 
 
-for f in find_music('music', 'emp3'):
-    print(f)
+# for f in find_music('music', 'emp3'):
+#     print(f)
 # can use generators in for loop and take care of creating the generator, for loop creates generator and calling next
 
 # could have done this too
 #
-# my_music_files = find_music('music', 'emp3')
-#
-# for f in my_music_files:
-#     print(f)
+my_music_files = find_music('music', 'emp3')
+
+for f in my_music_files:
+    id3r = id3reader.Reader(f)
+    print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
+        id3r.get_value('performer'),
+        id3r.get_value('album'),
+        id3r.get_value('track'),
+        id3r.get_value('title')
+    ))
