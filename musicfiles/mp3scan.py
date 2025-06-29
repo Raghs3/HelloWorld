c67c636b269ab7c8f51b97a5b988358703eb8321
Raghs3 <tinku.minku2803@gmail.com>
2025-06-29 22:46:26 +0530

    Day 223 - Generator with computer files

--- musicfiles/mp3scan.py
+++ musicfiles/mp3scan.py
@@ -19,7 +19,11 @@ def find_music(start, extension):
 my_music_files = find_music('music', 'emp3')
 
 for f in my_music_files:
-    id3r = id3reader.Reader(f)
+    try:
+        id3r = id3reader.Reader(f)
+    except:
+        pass
+
     print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
         id3r.get_value('performer'),
         id3r.get_value('album'),
