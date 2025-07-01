acfdac6cbeb7b2fd9c63f3e59fa030ff0e032108
Raghs3 <tinku.minku2803@gmail.com>
2025-07-01 19:00:35 +0530

    Day 225 - List Comprehensions

--- musicfiles/listcomp.py
+++ musicfiles/listcomp.py
@@ -2,7 +2,12 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# squares = [number ** 2 for number in numbers]  # the `[ ]` is the `list` comprehension
-squares = [number ** 2 for number in range(1, 7)]  # same as above
+number = int(input("Please enter a number, and I'll tell you it's square: "))
+# works for comprehension not with for loops
+squares = [number ** 2 for number in numbers]  # the `[ ]` is the `list` comprehension
+# squares = [number ** 2 for number in range(1, 7)]  # same as above
 # bracket of comprehension gives that comprehension for ex: `{ }` gives set comprehension
-print(squares)
+# print(squares)
+
+index_pos = numbers.index(number)
+print(squares[index_pos])
