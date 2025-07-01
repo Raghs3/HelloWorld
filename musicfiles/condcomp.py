e0a9dce17f3bc01c18e5a1e69d60cc1c711b0496
Raghs3 <tinku.minku2803@gmail.com>
2025-07-01 20:48:18 +0530

    Day 225 - List Comprehensions

--- musicfiles/condcomp.py
+++ musicfiles/condcomp.py
@@ -18,5 +18,7 @@ for meal in menu:
         meals.append("a meal was skipped")
 print(meals)  # same as comprehension
 
-meals = [meal for meal in menu if "spam" not in meal]  # can't have else when if used (filter)
+# meals = [meal for meal in menu if "spam" not in meal]  # can't have else when if used (filter)
+# meals = [meal for meal in menu if "spam" not in meal if "chicken" not in meal]  # makes it simpler to read
+meals = [meal for meal in menu if "spam" not in meal and "chicken" not in meal]
 print(meals)
