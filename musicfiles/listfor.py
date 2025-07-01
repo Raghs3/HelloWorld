7af3bb3a5ae967ffc05f8ba8c20e1400caf5707d
Raghs3 <tinku.minku2803@gmail.com>
2025-07-01 18:59:25 +0530

    Day 225 - List Comprehensions

--- musicfiles/listfor.py
+++ musicfiles/listfor.py
@@ -2,8 +2,11 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+number = int(input("Please enter a number, and I'll tell you it's square: "))
+# whtever number u input always returns 36 as same var also used in for loop
 squares = []
 for number in numbers:
     squares.append(number ** 2)
 
-print(squares)
+index_pos = numbers.index(number)
+print(squares[index_pos])
