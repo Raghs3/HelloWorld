1386b42539291792920b6cb7efe01e610255381e
Raghs3 <tinku.minku2803@gmail.com>
2025-07-01 18:02:10 +0530

    Day 225 - List Comprehensions

--- musicfiles/listcomp.py
+++ musicfiles/listcomp.py
@@ -2,6 +2,6 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-squares = [number ** 2 for number in numbers]
-
+squares = [number ** 2 for number in numbers]  # the `[ ]` is the `list` comprehension
+# bracket of comprehension gives that comprehension for ex: `{ }` gives set comprehension
 print(squares)
