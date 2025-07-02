9c930cb5d22cccd549f76346495f9cf7094ec98d
Raghs3 <tinku.minku2803@gmail.com>
2025-07-02 21:55:03 +0530

    Day 225 - List Comprehensions

---
+++ musicfiles/condcomp1.py
@@ -0,0 +1,22 @@
+menu = [
+    ["egg", "spam", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+    ["spam", "egg", "sausage", "spam"],
+    ["chicken", "chips"]
+]
+
+meals = []
+for meal in menu:
+    if "spam" not in meal:
+        meals.append(meal)
+    else:
+        meals.append("a meal was skipped")
+print(meals)  # same as comprehension
+
+meals = [meal for meal in menu if "spam" not in meal]
+print(meals)
