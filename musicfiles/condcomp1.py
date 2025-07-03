5ec6101ea1ce61eed95871343e063a7ba8c2017e
Raghs3 <tinku.minku2803@gmail.com>
2025-07-03 16:24:30 +0530

    Day 227 - List Comprehensions

--- musicfiles/condcomp1.py
+++ musicfiles/condcomp1.py
@@ -10,18 +10,20 @@ menu = [
     ["chicken", "chips"]
 ]
 
-meals = []
-for meal in menu:
-    if "spam" not in meal:
-        meals.append(meal)
-    else:
-        meals.append("a meal was skipped")
-print(meals)  # same as comprehension
-
-meals = [meal if "spam" not in meal else "a meal skipped" for meal in menu]
-print(meals)
-
-x = 12
-expression = "Twelve" if x == 12 else "unknown"  # conditional statement like this use in comprehension for getting result like for loop
-print(expression)
+# meals = []
+# for meal in menu:
+#     if "spam" not in meal:
+#         meals.append(meal)
+#     else:
+#         meals.append("a meal was skipped")
+# print(meals)  # same as comprehension
+#
+# meals = [meal if "spam" not in meal else "a meal skipped" for meal in menu]
+# print(meals)
+#
+# x = 12
+# expression = "Twelve" if x == 12 else "unknown"  # conditional statement like this use in comprehension for getting result like for loop
+# print(expression)
 # [expression iteration]
+for meal in menu:
+    print(meal, "contains chicken" if "chicken" in meal else "contains bacon" if "bacon" in meal else "contains egg")
