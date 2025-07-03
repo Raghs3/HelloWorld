d81894bfff36017a18042e6f40a98f7513e6f847
Raghs3 <tinku.minku2803@gmail.com>
2025-07-03 19:25:40 +0530

    Day 227 - List Comprehensions

--- Comprehensions/nested_challenge.py
+++ Comprehensions/nested_challenge.py
@@ -8,9 +8,12 @@ for i in range(1, 11):
     for j in range(1, 11):
         print(i, i * j)
 
-
-for table in [[(i, i*j) for j in range(1, 11)] for i in range(1, 11)]:
-    print(table)
-
 times = [(i, i*j) for i in range(1, 11) for j in range(1, 11)]
 print(times)
+
+for x, y in [(i, i*j) for i in range(1, 11) for j in range(1, 11)]:
+    print(x, y)
+
+
+# for table in [[(i, i*j) for j in range(1, 11)] for i in range(1, 11)]:
+#     print(table)
