32012d950480f4a3744dc49193b6d3fb26f8c7bd
Raghs3 <tinku.minku2803@gmail.com>
2025-07-03 16:30:15 +0530

    Day 227 - List Comprehensions

--- musicfiles/condcomp1.py
+++ musicfiles/condcomp1.py
@@ -42,3 +42,7 @@ for meal in menu:
         if item in meal:
             print("{} contains {}".format(meal, item))
             break
+# fizzbuzz conditional statement
+for x in range(1, 31):
+    fizzbuzz = "fizz buzz" if x % 15 == 0 else "fizz" if x % 3 == 0 else "buzz" if x % 5 == 0 else str(x)
+    print(fizzbuzz)
