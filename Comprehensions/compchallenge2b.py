944109d551302cf1eed96030f17e3e23dff5b37c
Raghs3 <tinku.minku2803@gmail.com>
2025-07-05 20:24:45 +0530

    Day 230 - List Comprehensions

--- Comprehensions/compchallenge2b.py
+++ Comprehensions/compchallenge2b.py
@@ -1,3 +1,5 @@
+import timeit
+
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
              2: "You are at the top of a hill",
@@ -14,6 +16,7 @@ exits = {0: {"Q": 0},
 
 print("nested for loops")
 print("----------------")
+nested_loop = """\
 for loc in sorted(locations):
     exits_to_destination_1 = []
     for xit in exits:
@@ -21,26 +24,25 @@ for loc in sorted(locations):
             exits_to_destination_1.append((xit, locations[xit]))
     print("Locations leading to {}".format(loc), end='\t')
     print(exits_to_destination_1)
-
+"""
 print()
 
 print("List comprehension inside a for loop")
 print("------------------------------------")
+loop_comp = """\
 for loc in sorted(locations):
     exits_to_destination_2 = [(xit, locations[xit]) for xit in exits if loc in exits[xit].values()]
     print("Locations leading to {}".format(loc), end='\t')
     print(exits_to_destination_2)
-
+"""
 print()
 
 print("nested comprehension")
 print("--------------------")
+nested_comp = """\
 exits_to_destination_3 = [[(xit, locations[xit]) for xit in exits if loc in exits[xit].values()]
                           for loc in sorted(locations)]
-# print(exits_to_destination_3)
-#
-# print()
-
 for index, loc in enumerate(exits_to_destination_3):
     print("Locations leading to {}". format(index), end='\t')
     print(loc)
+"""
