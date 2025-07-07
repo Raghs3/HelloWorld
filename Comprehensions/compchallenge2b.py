ba8c72b75fc5d1e58d8c109d58e14a728bc8737e
Raghs3 <tinku.minku2803@gmail.com>
2025-07-07 21:03:01 +0530

    Day 232 - List Comprehensions

--- Comprehensions/compchallenge2b.py
+++ Comprehensions/compchallenge2b.py
@@ -1,6 +1,7 @@
 import timeit
 
 setup = """\
+gc.enable()
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
              2: "You are at the top of a hill",
