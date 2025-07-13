a8598d297d47c801278f00e83e1c40bc8c4559dc
Raghs3 <tinku.minku2803@gmail.com>
2025-07-13 22:54:17 +0530

    Day 239 - map function

--- map/map_intro.py
+++ map/map_intro.py
@@ -1,20 +1,33 @@
 text = "what have the romans ever done for us"
 
-capitals = [char.upper() for char in text]
-print(capitals)
+
+def comp_caps():
+    capitals = [char.upper() for char in text]
+    return capitals
 
 # use map
-map_capitals = list(map(str.upper, text))
-print(map_capitals)
+def map_caps():
+    map_capitals = list(map(str.upper, text))
+    return map_capitals
 
-words = [word.upper() for word in text.split(' ')]
-print(words)
+
+def comp_words():
+    words = [word.upper() for word in text.split(' ')]
+    return words
 
 # use map
-map_words = list(map(str.upper, text.split(' ')))
-print(map_words)
+def map_words():
+    map_w = list(map(str.upper, text.split(' ')))
+    return map_w
 
-for x in map(str.upper, text.split(' ')):  # directly iterate over map iterable object instead of converting iterable to list
-    print(x)
+# for x in map(str.upper, text.split(' ')):  # directly iterate over map iterable object instead of converting iterable to list
+#     print(x)
 
 # comprehensions are easier to write and read than map, need to test performance
+
+
+if __name__ == '__main__':
+    print(comp_caps())
+    print(map_caps())
+    print(comp_words())
+    print(map_words())
