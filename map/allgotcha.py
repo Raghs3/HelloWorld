b400dfb909d385276a18f3e08c1d94cd58e2199b
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 15:38:09 +0530

    Day 240 - map function

--- map/allgotcha.py
+++ map/allgotcha.py
@@ -1,4 +1,7 @@
 entries = []
 
-print("all: {}".format(all(entries)))  # gives true in empty list, documented behaviour(rem)
-print("any: {}".format(any(entries)))
+if entries:
+    print("all: {}".format(all(entries)))  # gives true in empty list, documented behaviour(rem)
+else:
+    print(False)
+    print("any: {}".format(any(entries)))
