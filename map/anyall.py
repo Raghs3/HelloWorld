b61e9bf4daff76c99c5cc45bfa5ec68e96b4de1f
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 15:14:25 +0530

    Day 240 - map function

--- map/anyall.py
+++ map/anyall.py
@@ -1,4 +1,9 @@
 entries = [1, 2, 3, 4, 5]
 
 print("all: {}".format(all(entries)))
-print("any: {}".format(any(entries)))  # both return true
+print("any: {}".format(any(entries)))  # both return true # because all/any value in entries is true value
+
+print("Iterable with a 'False' value")
+entries_with_zero = [1, 2, 0, 4, 5]
+print(f"all: {all(entries_with_zero)}")  # now return false
+print(f"any: {any(entries_with_zero)}")
