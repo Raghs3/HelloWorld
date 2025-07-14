fde54581735cda90a863b6e8eacbd6689f3a5dcd
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 15:38:31 +0530

    Day 240 - map function

--- map/allgotcha.py
+++ map/allgotcha.py
@@ -1,6 +1,6 @@
 entries = []
 
-if entries:
+if entries:  # here it evaluates as false
     print("all: {}".format(all(entries)))  # gives true in empty list, documented behaviour(rem)
 else:
     print(False)
