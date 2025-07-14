76c48bbb482fa1a48c0af792f1c388c2048a83d1
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 15:39:45 +0530

    Day 240 - map function

--- map/allgotcha.py
+++ map/allgotcha.py
@@ -5,3 +5,7 @@ if entries:  # here it evaluates as false
 else:
     print(False)
     print("any: {}".format(any(entries)))
+
+# result = entries and all(entries)  # gives empty list not false, have to be explicit
+result = bool(entries) and all(entries)
+print(result)
