a0bc1a7e555baa42ad0918c0ddaa20c4c4a13835
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 14:25:28 +0530

    Day 240 - map function

--- map/filtertest.py
+++ map/filtertest.py
@@ -1,3 +1,4 @@
+import timeit
 menu = [
     ["egg", "spam", "bacon"],
     ["egg", "sausage", "bacon"],
@@ -16,8 +17,11 @@ for meal in menu:
 
 print("-" * 40)
 
-meals = [meal for meal in menu if "spam" not in meal]
-print(meals)
+
+def spamless_comp():
+    meals = [meal for meal in menu if "spam" not in meal]
+    return meals
+
 
 print("-" * 40)
 
@@ -26,6 +30,13 @@ def not_spam(meal_list: list):
     return "spam" not in meal_list
 
 
-spamless_meals = list(filter(not_spam, menu))  # need to create a function that does filtering
-print(spamless_meals)  # filter takes 2 args, fn that does filtering and iterable to iterate over
-# output identical to comp
+def spamless_filter():
+    spamless_meals = list(filter(not_spam, menu))  # need to create a function that does filtering
+    return spamless_meals  # filter takes 2 args, fn that does filtering and iterable to iterate over
+    # output identical to comp
+
+if __name__ == "__main__":
+    print(spamless_comp())
+    print(spamless_filter())
+    print(timeit.timeit(spamless_comp, number=100000))
+    print(timeit.timeit(spamless_filter, number=100000))  # taking nearly twice the time
