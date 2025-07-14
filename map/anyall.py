9689159aeedf6866021995ab624e993fefaf9ef7
Raghs3 <tinku.minku2803@gmail.com>
2025-07-14 15:20:13 +0530

    Day 240 - map function

--- map/anyall.py
+++ map/anyall.py
@@ -7,3 +7,17 @@ print("Iterable with a 'False' value")
 entries_with_zero = [1, 2, 0, 4, 5]
 print(f"all: {all(entries_with_zero)}")  # now return false
 print(f"any: {any(entries_with_zero)}")
+
+
+print()
+print("Values interpreted as False in Python")
+print("""False: {0}
+None: {1}
+0: {2}
+0.0: {3}
+empty list []: {4}
+empty tuple (): {5}
+empty string '': {6}
+empty string "": {7}
+empty mapping {{}}: {8}
+""".format(False, bool(None), bool(0), bool(0.0), bool([]), bool(()), bool(''), bool(""), bool({})))
