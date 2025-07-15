83393784c01039da19a6db08d4c14a647fd47152
Raghs3 <tinku.minku2803@gmail.com>
2025-07-15 12:43:58 +0530

    Day 241 - map function

--- map/namedtupletest.py
+++ map/namedtupletest.py
@@ -1,3 +1,5 @@
+from tkinter.scrolledtext import example
+
 from data import basic_plants_list, plants_list
 
 print(plants_list[0])  # output - P(name=............)  # so avoid having tuple name and typename diff
@@ -7,3 +9,10 @@ print(plants_list[0])  # output - P(name=............)  # so avoid having tuple 
 
 for plant in plants_list:
     print(plant.name, plant[1])  # makes readable and don't need to rem the posn, but we can still use indexing if we want
+# named tuples have three extra features normal tuples don't have and are prefixed with `_`
+print()
+
+example = plants_list[0]
+print(example)
+example = example._replace(lifecycle='Annual')  # changes value at lifecycle field
+print(example)
