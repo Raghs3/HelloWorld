1016df75c34b5c26db1b31d1dc1f08f93c5fdfb8
Raghs3 <tinku.minku2803@gmail.com>
2025-07-15 12:39:46 +0530

    Day 241 - map function

--- map/namedtupletest.py
+++ map/namedtupletest.py
@@ -1,3 +1,6 @@
 from data import basic_plants_list, plants_list
 
 print(plants_list[0])  # output - P(name=............)  # so avoid having tuple name and typename diff
+
+for plant in basic_plants_list:
+    print(plant[0])
