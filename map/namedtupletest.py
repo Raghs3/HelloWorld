7205cd89c8ebcd21ba63bf431388e8911cbf3f38
Raghs3 <tinku.minku2803@gmail.com>
2025-07-15 12:40:43 +0530

    Day 241 - map function

--- map/namedtupletest.py
+++ map/namedtupletest.py
@@ -2,5 +2,8 @@ from data import basic_plants_list, plants_list
 
 print(plants_list[0])  # output - P(name=............)  # so avoid having tuple name and typename diff
 
-for plant in basic_plants_list:
-    print(plant[0])
+# for plant in basic_plants_list:
+#     print(plant[0])
+
+for plant in plants_list:
+    print(plant.name, plant[1])  # makes readable and don't need to rem the posn, but we can still use indexing if we want
