fcd26b618f3d44f9bee3de030f481cd6f5633a10
Raghs3 <tinku.minku2803@gmail.com>
2025-07-15 12:35:08 +0530

    Day 241 - map function

--- map/data.py
+++ map/data.py
@@ -11,7 +11,7 @@ people = [
 
 # Named Tuples are described in the documentation
 # https://docs.python.org/3/library/collections.html#collections.namedtuple
-
+# field_names can't start with underscore
 Plant = namedtuple('Plant', ['name', 'scientific_name', 'lifecycle', 'plant_type'])  # change to P to see how it affects then undo changes
 PlantDetails = namedtuple('PlantDetails', ['scientific_name', 'lifecycle', 'plant_type'])
 # variable name and typename same to avoid confusion
