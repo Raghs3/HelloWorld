1cb6d4f18824339266dee6717848b39108d2495c
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 16:16:57 +0530

    Day 242 - map function

--- map/anycomprehension.py
+++ map/anycomprehension.py
@@ -16,3 +16,8 @@ if any(plant.plant_type == "Grass" for plant in plants_dict.values()):
     print("This dict contains grasses")
 else:
     print("No grasses in the dict")
+
+if any(plants_dict[key].plant_type == "Grass" for key in plants_dict):
+    print("This dict contains grasses")
+else:
+    print("No grasses in the dict")
