5d643888707658fb48518fbf94031859c5ac00a1
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 18:25:42 +0530

    Day 242 - lambda function

--- map/planting_instructions.py
+++ map/planting_instructions.py
@@ -1,1 +1,6 @@
 from data import plants_list
+
+with open('planting_instructions.txt', 'w', encoding='utf-8') as output_file:
+    for plant in plants_list:
+        where_to_plant = 'window box' if plant.lifecycle == 'Perennial' else 'garden'
+        print(f"{plant.name}: {where_to_plant}", file=output_file)
