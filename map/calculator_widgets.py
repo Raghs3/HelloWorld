92a930c0ee6c439bd0b6bbaaedf7a605a36da429
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 19:24:21 +0530

    Day 242 - lambda function

--- map/calculator_widgets.py
+++ map/calculator_widgets.py
@@ -4,3 +4,8 @@ import tkinter.messagebox
 
 class CalculatorButton(tk.Button):
     """A button with callback, to be used with a CalculatorGrid widget"""
+
+    def __init__(self, master, callback=None, **kwargs):
+        # self.callback = kwargs.pop('callback', None)
+        self.callback = callback
+        super().__init__(master, **kwargs)
