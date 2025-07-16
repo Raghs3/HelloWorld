e84f53a7f41e6b3eec53be5dde9c6a9e388c546a
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 18:40:56 +0530

    Day 242 - lambda function

--- map/planting_instructions.py
+++ map/planting_instructions.py
@@ -8,6 +8,17 @@ from data import plants_list
 #         else:
 #             where_to_plant = 'garden'
 
+
+def sort_perennials(item) -> str:
+    if item.lifecycle.casefold() == "perennial":
+        return '1' + item.name
+    else:
+        return '0' + item.name
+
+
+plants_list.sort(key=sort_perennials)
+
+
 with (open('planting_instructions.txt', 'w', encoding='utf-8') as output_file):
     for plant in plants_list:
         where_to_plant, who = ('window box', 'me') if plant.lifecycle == 'Perennial' \
