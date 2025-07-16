7b1cde6362f39d85dd49425e07df0e5938a0d1eb
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 13:04:02 +0530

    Day 242 - map function

--- map/anycomprehension.py
+++ map/anycomprehension.py
@@ -1,6 +1,8 @@
 from data import people, basic_plants_list, plants_list
 
-if all([person[1] for person in people]):  # all people don't have email address
+# people = []  # rem the gotcha, all returns true if empty list
+
+if bool(people) and all([person[1] for person in people]):  # all people don't have email address
     print("Sending email")
 
 else:
