6539b9e7b707e6d2ea3aed517904786afa3e916a
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 16:32:36 +0530

    Day 242 - map function

--- map/lambda_example.py
+++ map/lambda_example.py
@@ -5,5 +5,5 @@ def sort_key(d: dict) -> str:
     return d['rank']
 
 
-medals_table.sort(key=sort_key)
+medals_table.sort(key=sort_key)  # cant make it so the fn asks user for field as in sort function cant put params, so lambda useful here
 print(medals_table)
