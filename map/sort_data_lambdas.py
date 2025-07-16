e513259b62b85826df247ac93785f703b440ecc5
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 20:21:21 +0530

    Day 242 - lambda function

--- map/sort_data_lambdas.py
+++ map/sort_data_lambdas.py
@@ -15,7 +15,7 @@ while True:
 
     choice = input('Please select an option: ').upper()
 
-    description, function, reverse = options.get(choice)
+    description, function, reverse = options.get(choice, (None, None, None))
     if description:
         medals_table.sort(key=function, reverse=reverse)
     else:
