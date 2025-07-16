0c810b95a2ceccb9c8b9dd0a87c1bd3650981199
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 16:12:52 +0530

    Day 242 - map function

--- map/anycomprehension.py
+++ map/anycomprehension.py
@@ -4,6 +4,10 @@ from data import people, basic_plants_list, plants_list
 
 if bool(people) and all([person[1] for person in people]):  # all people don't have email address
     print("Sending email")
-
 else:
     print("User must edit the list of recipients")
+
+if any([plant.plant_type == "Grass" for plant in plants_list]):
+    print("This pack contains grass")
+else:
+    print("No grasses in this pack")
