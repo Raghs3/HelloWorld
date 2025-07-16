b9d768a31158a8d47d7433d7aec1a4d007776eee
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 19:28:43 +0530

    Day 242 - lambda function

--- map/calculator_widgets.py
+++ map/calculator_widgets.py
@@ -14,3 +14,21 @@ class CalculatorButton(tk.Button):
     def on_click(self):
         if self.callback:
             self.callback(self['text'])
+
+
+def test():
+    def clicked(caption: str):
+        print(f'{caption} was clicked')
+
+    main_window = tk.Tk()
+    main_window.title("CalculatorButton test")
+    main_window.geometry('640x480')
+
+    btn = CalculatorButton(main_window, callback=clicked, text='Test')
+    btn.grid(row=0, column=0, sticky='nsew')
+
+    main_window.mainloop()
+
+
+if __name__ == '__main__'
+    test()
