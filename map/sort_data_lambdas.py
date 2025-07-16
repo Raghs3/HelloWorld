705de4f818861bda17b06d360bdd8264126733fa
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 20:20:48 +0530

    Day 242 - lambda function

--- map/sort_data_lambdas.py
+++ map/sort_data_lambdas.py
@@ -1,11 +1,11 @@
 from medals_data import medals_table
 
 options = {
-    'C': ('country',),
-    'G': ('gold medals',),
-    'S': ('silver medals',),
-    'B': ('bronze medals',),
-    'R': ('rank',),
+    'C': ('country', lambda d: d['country'], False),
+    'G': ('gold medals', lambda d: d['gold'], True),
+    'S': ('silver medals', lambda d: d['silver'], True),
+    'B': ('bronze medals', lambda d: d['bronze'], True),
+    'R': ('rank', lambda d: d['rank'], False),
 }
 
 while True:
@@ -15,16 +15,9 @@ while True:
 
     choice = input('Please select an option: ').upper()
 
-    if choice == 'C':
-        medals_table.sort(key=lambda d: d['country'])
-    elif choice == 'G':
-        medals_table.sort(key=lambda d: d['gold'], reverse=True)
-    elif choice == 'S':
-        medals_table.sort(key=lambda d: d['silver'], reverse=True)
-    elif choice == 'B':
-        medals_table.sort(key=lambda d: d['bronze'], reverse=True)
-    elif choice == 'R':
-        medals_table.sort(key=lambda d: d['rank'])
+    description, function, reverse = options.get(choice)
+    if description:
+        medals_table.sort(key=function, reverse=reverse)
     else:
         break
 
