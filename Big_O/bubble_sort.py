95851c9da854bf5018141db254535449e7518089
Raghs3 <tinku.minku2803@gmail.com>
2025-07-16 22:36:26 +0530

    Day 242 - Big O

--- Big_O/bubble_sort.py
+++ Big_O/bubble_sort.py
@@ -4,19 +4,26 @@ def bubble_sort(data: list) -> None:
     comparison_count = 0
 
     for i in range(n - 1):
+        swapped = False
         for j in range(n - i - 1):  # improved bubble sort algo, fewer comparisons
             comparison_count += 1
             if data[j] > data[j + 1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
-
+                swapped = True
         print(f"End of pass {i}. `data` is now {data}")
+        if not swapped:
+            # The last pass resulted in no swaps.
+            # The data is now sorted.
+            break
+
     print(f"Comparison_count is {comparison_count}")
 
 
 # numbers = [3, 2, 4, 1, 5, 7, 6]
-numbers = [7, 6, 5, 4, 3, 2, 1]  # 21 passes instead of 36
-numbers = list(range(70, 0, -1))
-print(len(numbers))
+# numbers = [7, 6, 5, 4, 3, 2, 1]  # 21 passes instead of 36
+# numbers = list(range(70, 0, -1))
+# print(len(numbers))
+numbers = [1, 2, 3, 4, 6, 5, 7]
 
 print(f"Sorting {numbers}")
 bubble_sort(numbers)
